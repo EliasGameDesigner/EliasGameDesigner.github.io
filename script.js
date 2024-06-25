e5fe484540d71122b9616170001e69adf177a342
EliasGameDesigner <117873459+EliasGameDesigner@users.noreply.github.com>
2024-06-24 22:31:05 -0300

    versao 1

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function abrirPag(a){
+    console.log(a)
+    console.log("FOi")
+    let localPag = document.querySelector('html')
+    let pag = new XMLHttpRequest()
+
+    pag.onreadystatechange = () =>{
+        if(pag.readyState == 4 && pag.status == 200){
+            localPag.innerHTML = pag.response
+        }
+    }
+
+    pag.open('GET', `Projetos/${a}.html`)
+    pag.send()
+}
